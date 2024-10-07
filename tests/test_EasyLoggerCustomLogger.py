2a690fd8f2b807406b37b627724c2b5d8910e51b
Andrew McSparron <Amcsparron@albanyny.gov>
2024-10-07 08:25:19 -0400

    Add tests for conditional message printing in logger
    Introduced tests to verify message printing behavior across all log levels, ensuring correct function calls when `print_msg` is toggled. Deprecated the internal print call test as its functionality is covered by the new tests.

--- tests/test_EasyLoggerCustomLogger.py
+++ tests/test_EasyLoggerCustomLogger.py
@@ -6,6 +6,50 @@ from EasyLoggerAJM.EasyLoggerAJM import EasyLogger, _EasyLoggerCustomLogger
 class TestEasyLoggerCustomLogger(unittest.TestCase):
     def setUp(self):
         self.logger = EasyLogger().UseLogger().logger  # ._EasyLoggerCustomLogger("TestLogger")
+        self.log_methods = {
+            'info': self.logger.info,
+            'debug': self.logger.debug,
+            'warning': self.logger.warning,
+            'error': self.logger.error,
+            'critical': self.logger.critical
+        }
+
+    @patch('builtins.print')
+    def test_print_msg_for_all_log_levels(self, mock_print):
+        should_print = True
+        for level, method in self.log_methods.items():
+            with self.subTest(level=level):
+                with patch.object(_EasyLoggerCustomLogger, '_print_msg',
+                                  wraps=_EasyLoggerCustomLogger._print_msg) as mock_print_msg:
+                    method("Test message", print_msg=should_print)
+                    mock_print_msg.assert_called_once_with("Test message", print_msg=should_print)
+                    mock_print.assert_called_once_with("Test message")
+                    mock_print.reset_mock()
+
+    @patch('builtins.print')
+    def test_not_print_msg_for_all_log_levels(self, mock_print):
+        should_print = False
+        for level, method in self.log_methods.items():
+            with self.subTest(level=level):
+                with patch.object(_EasyLoggerCustomLogger, '_print_msg',
+                                  wraps=_EasyLoggerCustomLogger._print_msg) as mock_print_msg:
+                    method("Test message", print_msg=should_print)
+                    mock_print_msg.assert_called_once_with("Test message", print_msg=should_print)
+                    mock_print.assert_not_called()
+                    mock_print.reset_mock()
+
+    # these are now deprecated
+    @patch('builtins.print')
+    def test_internal_print_call_within_print_msg(self, mock_print):
+        with patch.object(_EasyLoggerCustomLogger, '_print_msg',
+                          wraps=_EasyLoggerCustomLogger._print_msg) as mock_print_msg:
+            # Call the info method to execute _print_msg
+            self.logger.info("Test message", print_msg=True)
+
+            # Assert that _print_msg was called within the scope
+            mock_print_msg.assert_called_once_with("Test message", print_msg=True)
+            # Now check that the print function was called within _print_msg
+            mock_print.assert_called_once_with("Test message")
 
     @patch.object(_EasyLoggerCustomLogger, '_print_msg')
     def test_info_print(self, mock_print):
