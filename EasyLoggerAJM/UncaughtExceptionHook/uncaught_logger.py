e70a241e3477bb9a53d978f9f38bd86841212de2
Andrew McSparron <Amcsparron@albanyny.gov>
2025-11-07 15:52:57 -0500

    Introduce `UncaughtLogger` for handling uncaught exceptions
    Added `UncaughtLogger` class to streamline logging uncaught exceptions with email notifications. Ensured it avoids file-based handlers and focuses solely on email-based alerts for cleaner log management. WIP

---
+++ EasyLoggerAJM/UncaughtExceptionHook/uncaught_logger.py
@@ -0,0 +1,50 @@
+from EasyLoggerAJM import OutlookEmailHandler, EasyLogger
+
+# FIXME: WIP
+class UncaughtLogger(EasyLogger):
+    def __init__(self, **kwargs):
+        # Initialize base logger (may attach default handlers)
+        kwargs.setdefault('log_spec', 'hourly')
+        super().__init__(**kwargs)
+        self.emailer = PyEmailer(False, False, logger=self.logger)
+        # Ensure this special logger only handles uncaught exceptions and does not interfere with others
+        self.logger.filters.clear()
+        self.logger.addFilter(UncaughtExceptionFilter())
+
+        self.logger.handlers = self.setup_clean_handlers()
+
+    def setup_clean_handlers(self):
+        # Remove any file-based handlers that the base class may have attached; we only want email here
+        # Keep non-file handlers (e.g., SMTP/email handlers) intact
+        cleaned_handlers = []
+        for h in list(self.logger.handlers):
+            try:
+                from logging import FileHandler
+                from logging.handlers import TimedRotatingFileHandler
+                is_file = isinstance(h, FileHandler) or isinstance(h, TimedRotatingFileHandler)
+            except Exception:
+                is_file = False
+            if not is_file:
+                cleaned_handlers.append(h)
+        return cleaned_handlers
+
+    def setup_email_handler(self, **kwargs):
+        email_msg = kwargs.pop('email_msg', self.emailer.email)
+        email_handler_class = kwargs.pop('email_handler_class', OutlookEmailHandler)
+        super().setup_email_handler(email_handler_class=email_handler_class,
+                                    logger_dir_path=self.log_location,
+                                    email_msg=email_msg, **kwargs)
+
+    def _set_logger_class(self, **kwargs):
+
+        return super()._set_logger_class(logger_name=kwargs.pop('logger_name',
+                                                                'uncaught_logger'), **kwargs)
+
+    def make_file_handlers(self, **kwargs):
+        """Override to disable creation of file handlers for the uncaught-exception logger."""
+        return None
+
+    def _set_initial_properties_value(self, **kwargs):
+        # noinspection PyTypeChecker
+        kwargs['log_spec'] = 'hourly'
+        super()._set_initial_properties_value(**kwargs)
