59c3b63f09109a34fb0ea56281ecb77f7bf7744e
Andrew McSparron <Amcsparron@albanyny.gov>
2026-06-10 12:47:35 -0400

    Add `CleanANSIFileFormatter` to sanitize log messages and handle ANSI sequences
    Introduced `CleanANSIFileFormatter` to clean log messages, ensuring only printable characters are retained and ANSI sequences are removed. Included manual argument formatting to avoid interpolation issues during logging.

--- EasyLoggerAJM/logger_parts/formatters.py
+++ EasyLoggerAJM/logger_parts/formatters.py
@@ -1,4 +1,6 @@
-from logging import Formatter
+import string
+from logging import Formatter, LogRecord
+from re import sub
 
 NO_COLORIZER = False
 try:
@@ -53,3 +55,56 @@ class ColorizedFormatter(Formatter):
         else:
             return self.colorizer.colorize(text=super().formatException(ei),
                                            color=self._get_record_color(ei), bold=True)
+
+
+class CleanANSIFileFormatter(Formatter):
+    """
+    Custom formatter to handle log record messages.
+
+    This class provides custom handling of log messages, particularly ensuring
+    that the messages are cleaned to contain only printable characters, and
+    any issues with interpolation of modified log messages are avoided. Inherits
+    from `Formatter`.
+    """
+
+    @staticmethod
+    def _manual_arg_format(record: LogRecord):
+        """ Format the message with its arguments before cleaning"""
+        if record.args:
+            # Manually format the message
+            record.msg = record.msg % record.args
+            # Optional: Clear the args to avoid reformatting issues downstream
+            record.args = None
+        return record
+
+    def format(self, record: LogRecord) -> str:
+        record = self._manual_arg_format(record)
+
+        # Clean the fully formatted log message
+        record.msg = self.clean_log_message(record.msg)
+
+        # Now use the parent class to complete formatting
+        return super().format(record)
+
+    @staticmethod
+    def _remove_ansi_escape_sequences(msg: str) -> str:
+        """Remove ANSI escape sequences from a string."""
+        # sub out any string that starts with [ and ends with m with ''
+        pattern = r"\[\w.*?m"
+        return sub(pattern, "", msg)
+
+    def clean_log_message(self, msg: str) -> str:
+        """ Ensures only characters that are printable per Unicode
+        and part of `string.printable` are retained. This covers
+        both common printable characters and certain Unicode
+        characters that might also be "printable" but aren't
+        in the ASCII set. The self._remove_ansi_escape_sequences()
+        method covers any leftovers from Colorizer. """
+
+        if not isinstance(msg, str):
+            return msg
+
+        # filter out any non-printable chars from the results of self._remove_ansi_escape_sequences
+        filtered_msg_list = filter(lambda x: x in string.printable and x.isprintable(),
+                                   self._remove_ansi_escape_sequences(msg))
+        return ''.join(filtered_msg_list)
